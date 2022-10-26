ab4c5d9055aee4bdf8a55319810cb7bacbd0b963
Keogyun <108053731+Keogyun@users.noreply.github.com>
2022-10-26 09:39:37 +0900

    Add files via upload

---
+++ 9-1_ex2/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Specialization from './chapter_12/9-1_ex2/Specialization';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <div>
+      <Specialization />
+    </div>
+);
