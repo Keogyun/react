012da97a41f782910dd2dacb1ab9373026816cd3
Keogyun <108053731+Keogyun@users.noreply.github.com>
2022-10-26 09:35:13 +0900

    Add files via upload

---
+++ 9-1_ex1/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PassingCount from './chapter_12/9-1_ex1/PassingCount';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <div>
+      <PassingCount />
+    </div>
+);
