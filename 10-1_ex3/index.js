81be9b564e1bae21e886066c06839a08ed8f3278
Keogyun <108053731+Keogyun@users.noreply.github.com>
2022-10-31 16:12:58 +0900

    Add files via upload

---
+++ 10-1_ex3/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PageColor from './chpater_13/PageColor.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <div>
+      <PageColor />
+    </div>
+);
