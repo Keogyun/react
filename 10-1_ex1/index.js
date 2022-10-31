1239d379a3b97b690860757492a9a620464e335d
Keogyun <108053731+Keogyun@users.noreply.github.com>
2022-10-31 16:05:32 +0900

    Add files via upload

---
+++ 10-1_ex1/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ContextApp from './chpater_13/ContextApp.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <div>
+      <ContextApp />
+    </div>
+);
